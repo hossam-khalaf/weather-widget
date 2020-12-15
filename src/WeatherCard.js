0c6c49c821827e037570acad5bd601f639496768
hossam-khalaf <hossamhassan303089@gmail.com>
2020-12-15 13:33:07 +0200

    basic weather card UI

---
+++ src/WeatherCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { WiCloud } from 'react-icons/wi'
+
+const WeatherCard = (props) => {
+	return (
+		<div className='card'>
+			<div className='location'>
+				<h1 className='city'>Sydney</h1>
+				<h3 className='country'>AU</h3>
+			</div>
+			<WiCloud className='icon' />
+
+			<h1 className='temp'>20 °C</h1>
+			<h3 className='cond'>Clouds</h3>
+		</div>
+	)
+}
+
+export default WeatherCard
