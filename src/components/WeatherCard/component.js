42daa390354db9f9a79c3d83120fae174165aef6
hossam-khalaf <hossamhassan303089@gmail.com>
2020-12-17 03:40:30 +0200

    emotion styled

--- src/components/WeatherCard/component.js
+++ src/components/WeatherCard/component.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import { WiCloud } from 'react-icons/wi'
+import styled from '@emotion/styled'
 
 const WeatherCard = (props) => {
+	const Card = styled.div`
+		background: linear-gradient(to bottom, blue, lightblue);
+		margin: 0 auto;
+		width: 200px;
+		height: 240px;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		align-items: center;
+		border-radius: 16px;
+		box-shadow: 1px 5px 10px 3px rgba(0, 0, 0, 0.164);
+	`
+
 	return (
-		<div className='card'>
+		<Card className='card'>
 			<div className='location'>
 				<h1 className='city'>Sydney</h1>
 				<h3 className='country'>AU</h3>
@@ -13,7 +27,7 @@ const WeatherCard = (props) => {
 
 			<h1 className='temp'>20 °C</h1>
 			<h3 className='cond'>Clouds</h3>
-		</div>
+		</Card>
 	)
 }
 
